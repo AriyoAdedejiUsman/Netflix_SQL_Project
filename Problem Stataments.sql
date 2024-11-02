4c5fea201512a13eadc64322db1fbdaf3917788a
AriyoAdedejiUsman <157654654+AriyoAdedejiUsman@users.noreply.github.com>
2024-11-02 19:16:20 +0100

    Add files via upload

---
+++ Problem Stataments.sql
@@ -0,0 +1,47 @@
+-- 15 Problem Statements
+
+1. Determine the Most Popular Genres by Country
+Identify the most popular genres in each country based on the number of shows and movies listed under each genre.
+
+2. Find the Most Common Rating for Movies and TV Shows
+Identify which rating (like PG, PG-13, R, etc.) appears the most frequently across Netflix content.
+
+3.List all movies released in  2019
+ Extract the movies that were released in the  year 2019, filtering by the release_year column.
+ 
+
+4. Find the Top 5 Countries with the Most Content on Netflix
+Determine which countries have contributed the most content to Netflixs catalog.
+
+5. Identify the Longest Movie or TV Show Duration
+Find the movie with the longest runtime or TV show with the most number of episodes.
+
+6. Find Content Added in the Last 5 Years
+List the content that has been added to Netflix within the last five years.
+
+7. Find All Movies/TV Shows by Director 'Steven Spielberg'
+Extract all movies or TV shows that were directed by Steven Spielberg.
+
+8. List All TV Shows with More Than 5 Seasons
+Identify TV shows that have more than 5 seasons.
+
+9. Count the Number of Content Items in Each Genre
+Count how many content items belong to each genre.
+
+10. Find the Average Content per Year Produced in Nigeria
+Calculate the average content per year that was produced in Nigeria.
+
+11. List All Movies That Are Documentaries
+Extract all movies classified as documentaries.
+
+12. Find All Content Without a Director
+Find the entries where no director is listed.
+
+13. Find Movies with Descriptions Containing Keywords ('AI', 'Sci-Fi', 'Space')
+Search for movies containing the keywords "AI," "Sci-Fi," or "Space" in the description.
+
+14. Find the Most Common Cast Members
+Identify the most frequently occurring actors/actresses across the dataset.
+
+15. Identify the Top 10 Countries Producing TV Shows
+Find out which countries have produced the most TV shows.
